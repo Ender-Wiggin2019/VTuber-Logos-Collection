8fb4991310a97b0fc7c93821bbfe558d89a4566a
senong <1120659490@qq.com>
2024-04-27 02:54:35 +0800

    feat: add toolbar fixed & add to-top-button

--- src/components/comp/search-bar.tsx
+++ src/components/comp/search-bar.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 export function SearchBar({ setSearch }: Props) {
   return (
-    <div className="relative w-full">
+    <div className="relative">
       <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
       <Input type="search" placeholder="Search logos..." className="pl-8 sm:w-[300px] md:w-[400px] lg:w-[500px]" onChange={(e) => setSearch(e.target.value)} />
     </div>
