50724cc3e6b9461f710cca2680c313d2c8f1428e
NoCrypt <57245077+NoCrypt@users.noreply.github.com>
2024-04-28 16:32:15 +0700

    feat: use icon for toolbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
-          enableSystem>
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
