29eb52fa6899997990310c204aef3b81f1b74b5e
FireIsGood <fireisadequate@gmail.com>
2024-05-23 01:05:34 -0700

    fix: page max width
    Sets a page max width now that the cards are not sized based on the
    number of tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -143,7 +143,7 @@ export default function Dashboard() {
             </DropdownMenuContent>
           </DropdownMenu> */}
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 max-w-screen-2xl">
           <div className="flex items-center">{/* <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1> */}</div>
           <div className="flex flex-1 items-start justify-center rounded-lg border--0 border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1">
             <div className="w-full z-10 items-center justify-between text-sm lg:flex overflow-y-auto">
