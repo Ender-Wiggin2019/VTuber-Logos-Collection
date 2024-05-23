9e1567421b0e30eab14d0bb99bdb38e29def2b96
FireIsGood <fireisadequate@gmail.com>
2024-05-23 12:57:49 -0700

    fix: sidebar size crushing logo
    Sidebar was set to have a maximum height of the page height which led to
    the logo getting crushed when sorting elements reloaded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Dashboard() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-full flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               {/* <Package2 className="h-6 w-6" /> */}
