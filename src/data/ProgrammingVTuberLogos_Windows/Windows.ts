f94d8e7a25f2488e80189c4e687c2fec1db7912a
Shiroiame-Kusu <kusu@kusu.moe>
2024-08-28 10:19:20 +0800

    Update.

--- src/data/ProgrammingVTuberLogos_Windows/Windows.ts
+++ src/data/ProgrammingVTuberLogos_Windows/Windows.ts
@@ -1,8 +1,10 @@
 import { Author, getCredit } from "../credits";
 import { IBrand } from "../type";
+import { BrandCategory } from "../categories";
 
 export const Windows: IBrand = {
     name: "Windows",
+    categories: [BrandCategory.OS],
     logos: [
       {
         url: "https://raw.githubusercontent.com//Shiroiame-Kusu/ProgrammingVTuberLogos-Windows/main/Windows/WindowsLogoShadow.png",
--- src/data/brand.ts
+++ src/data/brand.ts
@@ -2,13 +2,14 @@ import { CuteLogos } from "./CuteLogos";
 import { CuteVtubingThing } from "./CuteVtubingThing";
 import { DownloadedLogos } from "./DownloadedLogos";
 import { ProgrammingVTuberLogos } from "./ProgrammingVTuberLogos";
+import { ProgrammingVTuberLogos_Windows } from "./ProgrammingVTuberLogos_Windows";
 import { ServiceLogos } from "./ServiceLogos";
 import { VTuberLogos } from "./VtuberLogos";
 import { FindViaTwitterLogos } from "./brands";
 import { FunLogos } from "./FunLogos";
 import { IBrand } from "./type";
 
-const BRANDS: IBrand[] = [...CuteLogos, ...ProgrammingVTuberLogos, ...FindViaTwitterLogos, ...ServiceLogos, ...DownloadedLogos, ...CuteVtubingThing, ...VTuberLogos, ...FunLogos];
+const BRANDS: IBrand[] = [...CuteLogos, ...ProgrammingVTuberLogos, ...FindViaTwitterLogos, ...ServiceLogos, ...DownloadedLogos, ...CuteVtubingThing, ...VTuberLogos, ...FunLogos, ...ProgrammingVTuberLogos_Windows];
 
 BRANDS.sort((a, b) => a.name.localeCompare(b.name));
 
