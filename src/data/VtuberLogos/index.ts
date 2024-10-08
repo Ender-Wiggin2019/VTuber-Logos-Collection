00a2906255a6e41f5de7c934a71a32e8c4495bb3
AnwarAchilles <hudorianwar07@gmail.com>
2024-10-08 11:10:52 +0700

    Add CodeIgniter & NestJS logos, redesign PHP logo to meet community standards and make printable

--- src/data/VtuberLogos/index.ts
+++ src/data/VtuberLogos/index.ts
@@ -1,9 +1,13 @@
 import { LinuxMint } from "./LinuxMint";
 import { Php } from "./php";
+import { CodeIgniter } from "./Codeigniter";
+import { NestJS } from "./NestJS";
 
 import { IBrand } from "../type";
 
 export const VTuberLogos: IBrand[] = [
     LinuxMint,
-    Php
+    Php,
+    CodeIgniter,
+    NestJS,
 ];
